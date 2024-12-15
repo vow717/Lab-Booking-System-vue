e0a40cbc2683a49baf13218455e97e63073ab1bc
yue24413 <2312400876@qq.com>
2024-12-15 17:19:40 +0800

    管理员重置老师密码功能

--- src/datasource/datasourse.ts
+++ src/datasource/datasourse.ts
@@ -9,13 +9,22 @@ const labs: Lab[] = [
   { id: "7", name: '201', location:'主楼',capacity:60,createTime: '2046-05-10T12:00' },
 ]
 
-
+// export const ADMIN = 'R2md'
+// export const TEACHER = 'kU4T'
+//账号密码暂时不用约定的65位（难以模拟）
+const teachers: User[] = [
+  { id: "1", name: 'BO', account:'2024202401',password:"111111",role:"kU4T",phone:"12345678910",createTime: '2046-05-10T12:00' },
+  { id: "2", name: 'SUN', account:'2024202402',password:"22222",role:"kU4T",phone:"12345678911",createTime: '2046-04-10T09:00' },
+  { id: "3", name: 'YUE', account:'2024202501',password:"33333",role:"R2md",phone:"12345678912",createTime: '2046-06-10T21:30' },
+  { id: "4", name: 'CHEN', account:'2024202403',password:"44444",role:"kU4T",phone:"12345678913",createTime: '2046-06-13T18:43' },
+  { id: "5", name: 'WU', account:'2024202404',password:"5555",role:"kU4T",phone:"12345678914",createTime: '2046-05-10T12:00' },
+]
 
 
 
 export function listLabs() {
   return labs
 }
-export function listManagers() {
-    return managers
-  }
+export function listTeachers() {
+  return teachers
+}
