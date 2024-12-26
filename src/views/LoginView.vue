9c28bdd938a6329520d60e08d22def69032f2aff
vow717 <147890905+vow717@users.noreply.github.com>
2024-12-26 10:06:26 +0800

    验证码

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -14,10 +14,10 @@ import {
   Watermelon
 } from '@element-plus/icons-vue'
 
+import dentify from '@/components/dentify.vue'
 import type { User } from '@/datasource/type'
 import { CommonService } from '@/services/index'
-import { ref } from 'vue'
-
+import { onMounted, ref } from 'vue'
 const userForm = ref({
   account: '',
   password: ''
@@ -29,13 +29,46 @@ const resetUser = () => {
 }
 
 const loginF = async () => {
+  if (formLogin.value.code !== identifyCode.value) {
+    initCode()
+    throw new Error('验证码错误')
+  }
   const user: User = {
     account: userForm.value.account,
     password: userForm.value.password
   }
   resetUser()
+  initCode()
   await CommonService.loginGuardService(user)
 }
+const identifyCodes = ref('1234567890abcdefjhijklinopqrsduvwxyz')
+const identifyCode = ref('')
+const formLogin = ref({
+  code: ''
+})
+// 初始化验证码
+const initCode = () => {
+  identifyCode.value = ''
+  makeCode(identifyCodes.value, 4)
+}
+// 获取验证码的值
+const makeCode = (o: string, l: number) => {
+  for (let i = 0; i < l; i++) {
+    // 通过循环获取字符串内随机几位
+    identifyCode.value += o[randomNum(0, o.length)]
+  }
+}
+// 随机数字：用于当角标拿字符串的值
+const randomNum = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+const refreshCode = () => {
+  identifyCode.value = ''
+  makeCode(identifyCodes.value, 4)
+}
+onMounted(() => {
+  initCode()
+})
 </script>
 <template>
   <div>
@@ -72,14 +105,28 @@ const loginF = async () => {
               placeholder="请输入您的密码"
               :rules="[{ required: true, message: '密码不能为空', trigger: 'blur' }]" />
           </el-form-item>
+          <el-form-item>
+            <el-row :gutter="10">
+              <el-col :span="12">
+                <el-input placeholder="请输入验证码" v-model="formLogin.code"></el-input>
+              </el-col>
 
+              <!--验证码右边到底部-->
+              <el-col :span="8">
+                <div class="login-code" width="100%" @click="refreshCode">
+                  <!--验证码组件-->
+                  <dentify :identifyCode="identifyCode"></dentify>
+                </div>
+              </el-col>
+            </el-row>
+          </el-form-item>
           <el-form-item>
             <div style="position: relative; left: 300px">
               <el-button
                 type="primary"
                 :icon="Check"
                 @click="loginF"
-                :disabled="!userForm.account || !userForm.password">
+                :disabled="!userForm.account || !userForm.password || !formLogin.code">
                 登入
               </el-button>
               <el-button @click="resetUser()" :icon="Refresh">重置</el-button>
@@ -102,7 +149,7 @@ const loginF = async () => {
 }
 .d2 {
   position: relative;
-  height: 220px;
+  height: 280px;
   width: 480px;
   border-radius: 15px;
 
