54ddbd58a6e346f31f58b8ee38de03d8059e530f
vow717 <147890905+vow717@users.noreply.github.com>
2025-01-02 16:20:01 +0800

    update

--- src/views/admin/LabView.vue
+++ src/views/admin/LabView.vue
@@ -3,21 +3,12 @@ import { locationItems } from '@/datasource/const'
 import type { Lab, LabName } from '@/datasource/type'
 import { AdminService } from '@/services/AdminService'
 import EditLabVue from '@/views/admin/OperationLabView.vue'
-import { ref, watch } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import AddLabVue from './operation/AddLabVue.vue'
-const allLabs = await AdminService.listLabsService()
-const activeName = ref('0')
-console.log(allLabs.value)
-import { AdminService } from '@/services/AdminService';
-import { ref, watch, reactive, onMounted } from 'vue';
-import type { Lab, LabName } from '@/datasource/type';
-import { locationItems } from '@/datasource/const';
-import EditLabVue from '@/views/admin/OperationLabView.vue';
-import AddLabVue from './operation/AddLabVue.vue';
 
 // 使用 ref 来存储 allLabs，使其具有响应性
-const allLabs = ref<Lab[]>([]);
-const activeName = ref('0');
+const allLabs = ref<Lab[]>([])
+const activeName = ref('0')
 
 const locationName = ref<LabName>({
   DanQing: [],
@@ -27,44 +18,35 @@ const locationName = ref<LabName>({
   JiaJu: [],
   JiaoTong: []
 })
-//
-    DanQing: [],
-    ChengDong: [],
-    Zhu: [],
-    LinKe: [],
-    JiaJu: [],
-    JiaoTong: []
-});
 
 // 收集实验室并分类存储
 const collectLS = (lS: Lab[]) => {
-  lS.forEach((lab) => {
+  lS.forEach(lab => {
     if (lab.name?.slice(0, 2) == '丹青') {
-      locationName.value.DanQing?.push(lab);
+      locationName.value.DanQing?.push(lab)
     } else if (lab.name?.slice(0, 2) === '成栋') {
-      locationName.value.ChengDong?.push(lab);
+      locationName.value.ChengDong?.push(lab)
     } else if (lab.name?.slice(0, 2) === '主楼') {
-      locationName.value.Zhu?.push(lab);
+      locationName.value.Zhu?.push(lab)
     } else if (lab.name?.slice(0, 2) === '林科') {
-      locationName.value.LinKe?.push(lab);
+      locationName.value.LinKe?.push(lab)
     } else if (lab.name?.slice(0, 2) === '家具') {
-      locationName.value.JiaJu?.push(lab);
+      locationName.value.JiaJu?.push(lab)
     } else if (lab.name?.slice(0, 2) === '交通') {
-      locationName.value.JiaoTong?.push(lab);
+      locationName.value.JiaoTong?.push(lab)
     }
-  });
+  })
 
   // 对每个分类下的实验室进行排序，假设按 name 排序
   for (const key in locationName.value) {
     locationName.value[key as keyof LabName].sort((a, b) => {
       if (a.name && b.name) {
-        return a.name.localeCompare(b.name);
+        return a.name.localeCompare(b.name)
       }
-      return 0;
-    });
+      return 0
+    })
   }
-};
-
+}
 
 // 将 locationMap 变成响应式对象
 const locationMap = reactive({
@@ -74,39 +56,36 @@ const locationMap = reactive({
   LinKe: locationName.value.LinKe || [],
   JiaJu: locationName.value.JiaJu || [],
   JiaoTong: locationName.value.JiaoTong || []
-});
+})
 
+const currentLocationF = (v: string) => locationMap[v]
 
-const currentLocationF = (v: string) => locationMap[v];
-
-
-watch(allLabs, (newval) => {
+watch(allLabs, newval => {
   // 清空 locationName 中的数据
-  locationName.value.DanQing = [];
-  locationName.value.ChengDong = [];
-  locationName.value.Zhu = [];
-  locationName.value.LinKe = [];
-  locationName.value.JiaJu = [];
-  locationName.value.JiaoTong = [];
+  locationName.value.DanQing = []
+  locationName.value.ChengDong = []
+  locationName.value.Zhu = []
+  locationName.value.LinKe = []
+  locationName.value.JiaJu = []
+  locationName.value.JiaoTong = []
   // 重新调用 collectLS 对新数据进行分类和排序
-  collectLS(newval);
+  collectLS(newval)
   // 更新 locationMap 的属性
-  locationMap.DanQing = locationName.value.DanQing;
-  locationMap.ChengDong = locationName.value.ChengDong;
-  locationMap.Zhu = locationName.value.Zhu;
-  locationMap.LinKe = locationName.value.LinKe;
-  locationMap.JiaJu = locationName.value.JiaJu;
-  locationMap.JiaoTong = locationName.value.JiaoTong;
-  console.log(locationMap);
-});
-
+  locationMap.DanQing = locationName.value.DanQing
+  locationMap.ChengDong = locationName.value.ChengDong
+  locationMap.Zhu = locationName.value.Zhu
+  locationMap.LinKe = locationName.value.LinKe
+  locationMap.JiaJu = locationName.value.JiaJu
+  locationMap.JiaoTong = locationName.value.JiaoTong
+  console.log(locationMap)
+})
 
 // 在 onMounted 钩子中调用 listLabsService
 onMounted(async () => {
-  const res = await AdminService.listLabsService();
-  allLabs.value = res.value;
-  collectLS(allLabs.value);
-});
+  const res = await AdminService.listLabsService()
+  allLabs.value = res.value
+  collectLS(allLabs.value)
+})
 </script>
 <template>
   <div>
